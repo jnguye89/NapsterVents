330b0db362b0b8637093b8ca85c2e7f7695ae533
jnguye89 <Jackie@Jackies-MacBook-Pro.local>
2017-11-02 18:42:26 -0500

    firebase add

---
+++ javascript/main.js
@@ -0,0 +1,12 @@
+var config = {
+    apiKey: "AIzaSyDLnLEGIIHzvCIS2TRAjJIFE3_JtKYypt0",
+    authDomain: "project-7498a.firebaseapp.com",
+    databaseURL: "https://project-7498a.firebaseio.com",
+    projectId: "project-7498a",
+    storageBucket: "project-7498a.appspot.com",
+    messagingSenderId: "1007493758823"
+  };
+  firebase.initializeApp(config);
+
+
+ 
