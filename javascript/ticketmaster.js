4bb9a9d043963a60d7316cdcaa6b8507c6da8bfd
Tony Scott <tonyscott76@outlook.com>
2017-11-06 21:39:02 -0600

    dates / save events

--- javascript/ticketmaster.js
+++ javascript/ticketmaster.js
@@ -1,10 +1,14 @@
-// Author: James Scott
+// Begin Ticketmaster
 
+// Author: James Scott
 
 var artistNameIn;
 var zipCodeIn;
-var tmRootQueryURL = "https://app.ticketmaster.com/discovery/v2/";
+var tmRootQueryURL = "https://app.ticketmaster.com/discovery/v2/events.json";
+var tmQueryURL = "";
 var tmApiKey = "Q60tg8AuoiJG7UpD8Lk2jUH1vutlxRd0";
+var savedEvent = false;
+var tmEventDate = "";
 var tmEventHTML = "";
 
 // Add an on-click event listener for the Search button	
@@ -13,28 +17,30 @@ $("#nav-submit").on("click", function(event) {
 	artistNameIn = $("#artist-name").val().trim();
 	zipCodeIn = $("#zip-code").val().trim();
 
-	// Either the artist name or zip code must be entered
-	if (artistNameIn !== "" || zipCodeIn !== "") {
-		tmEvents = getTicketmasterInfo();
-	}
+	tmEvents = getTicketmasterInfo();
+	console.log("tmEvents: ", tmEvents);
+})
+
+$("#event-fav").on("click", ".fav-event-button", function(event) {
+	displaySavedEvent(event);
 })
 
 // Get the music events from Ticketmaster
 function getTicketmasterInfo() {
 
-	// console.log("artistNameIn: ", artistNameIn);
-	// console.log("zipCodeIn: ", zipCodeIn);
-	
+ 	savedEvent = false;
+
 	// The query URL for the AJAX call to Ticketmaster.
 	// The results are sorted by event date
-	tmQueryURL = tmRootQueryURL + "events.json?apikey=" + tmApiKey
-															+ "&source=ticketmaster"
-															+ "&radius=50"
-											 	   		+ "&classificationName=music"
+	tmQueryURL = tmRootQueryURL	+ "?keyword=" + artistNameIn
+															+ "&postalCode=" + zipCodeIn
+															+ "&radius=100"
 												   		+ "&sort=date,asc"
-												   		+ "&keyword=" + artistNameIn
-												   		+ "&postalCode=" + zipCodeIn;
+											 	   		+ "&classificationName=music"
+															+ "&apikey=" + tmApiKey;
 
+	console.log("getTicketmasterInfo() tmQueryURL: ", tmQueryURL);
+	
 	$.ajax(
 	{
 	  type:"GET",
@@ -43,8 +49,7 @@ function getTicketmasterInfo() {
 	  dataType: "json",
 	  success: function(json) {
 
-	  	// console.log("json.page.totalElements: ", json.page.totalElements);
-	  	console.log("JSON Returned From Tickmaster AJAX Call: ", json);
+	  	console.log("getTicketmasterInfo() Tickmaster JSON: ", json);
 
 	  	// Check if music events were returned
 	  	if (json.page.totalElements === 0) {
@@ -66,10 +71,13 @@ function displayEvents(tmEvents) {
 
 	var i = 0;
 	var tmEventState = "";
+	var currentDate = moment();
+	var tmEventDate;
 
-	$(".table").empty();
+	$("#event-info").empty();
 
-	tmEventHTML = "<table class='table'>";
+	tmEventHTML  = "<h2 class='panel-heading'>Ticketmaster Music Events</h2>";
+	tmEventHTML += "<table class='table'>";
 	tmEventHTML += "<thead>";
 	tmEventHTML += "<tr>"
 	tmEventHTML += "<th>Artist Name</th>";
@@ -84,55 +92,51 @@ function displayEvents(tmEvents) {
 
 	for (i = 0; i < tmEvents._embedded.events.length; i++) {
 
-		// console.log("Event On Sale: ", tmEvents._embedded.events[i].dates.status.code);
+		tmEventDate = moment(tmEvents._embedded.events[i].dates.start.localDate);
 
-		// Only display music events that have tickets for sale
-		if (tmEvents._embedded.events[i].dates.status.code === "onsale") {
-			// console.log("Event Artist Name: ", tmEvents._embedded.events[i].name);
-			// console.log("Event Date: ", tmEvents._embedded.events[i].dates.start.localDate);
-			// console.log("Event Venue: ", tmEvents._embedded.events[i]._embedded.venues[0].name);
-			// console.log("Event City: ", tmEvents._embedded.events[i]._embedded.venues[0].city.name);
-			// console.log("Event Country: ", tmEvents._embedded.events[i]._embedded.venues[0].country.countryCode);
-
-			if (tmEvents._embedded.events[i]._embedded.venues[0].country.countryCode === "US") {
-				tmEventState = tmEvents._embedded.events[i]._embedded.venues[0].state.stateCode;
-			}
-			else {
-				tmEventState = tmEvents._embedded.events[i]._embedded.venues[0].country.countryCode;
-			}
-			// console.log("tmEventState: ", tmEventState);
-
-			// console.log("Event URL: ", tmEvents._embedded.events[i].url);
-
-			// Build the HTML to display the Ticketmaster music event information
-			tmEventHTML += "<tr>";
-			tmEventHTML += "<td>" + tmEvents._embedded.events[i].name + "</td>";
-			tmEventHTML += "<td>" + tmEvents._embedded.events[i]._embedded.venues[0].name + "</td>";
-			tmEventHTML	+= "<td>" + tmEvents._embedded.events[i]._embedded.venues[0].city.name + ", " + tmEventState + "</td>";
-			tmEventHTML	+= "<td>" + tmEvents._embedded.events[i].dates.start.localDate + "</td>";
-			tmEventHTML	+= "<td><a href=" + tmEvents._embedded.events[i].url + " target=_blank>Ticketmaster Info</a></td>";
-			tmEventHTML += "<td><button type='submit' class='btn btn-default' id='save-event-submit' data-event-artist='" + tmEvents._embedded.events[i].name + "' data-event-id='" + tmEvents._embedded.events[i].id + "' onclick='saveEvent()'>Save Event</button></td>";
-			favoriteEventButton();
-			tmEventHTML	+= "</tr>";
-			
+		// Don't display music events in the past
+		if ((tmEventDate).isBefore(currentDate, 'day')) {
+			continue;
 		}
+
+		// Music events outside the US don't have a state code property.
+		// In those cases use the country code in place of the state code.
+		if (tmEvents._embedded.events[i]._embedded.venues[0].country.countryCode === "US") {
+			tmEventState = tmEvents._embedded.events[i]._embedded.venues[0].state.stateCode;
+		}
+		else {
+			tmEventState = tmEvents._embedded.events[i]._embedded.venues[0].country.countryCode;
+		}
+
+		// Build the HTML to display the Ticketmaster music event information
+		tmEventHTML += "<tr>";
+		tmEventHTML += "<td>" + tmEvents._embedded.events[i].name + "</td>";
+		tmEventHTML += "<td>" + tmEvents._embedded.events[i]._embedded.venues[0].name + "</td>";
+		tmEventHTML	+= "<td>" + tmEvents._embedded.events[i]._embedded.venues[0].city.name + ", " + tmEventState + "</td>";
+		tmEventHTML += "<td>" + moment(tmEvents._embedded.events[i].dates.start.localDate).format("MMMM Do YYYY") + "</td>";
+		tmEventHTML	+= "<td><a href=" + tmEvents._embedded.events[i].url + " target=_blank>Ticketmaster</a></td>";
+		
+		if (!savedEvent) {
+			tmEventHTML += "<td><button type='button' class='btn btn-primary btn-block' data-toggle='tooltip' title='Save This Event' id='save-event-submit' data-event-artist='" + tmEvents._embedded.events[i].name + "' data-event-id='" + tmEvents._embedded.events[i].id + "'><span class='glyphicon glyphicon-heart-empty'></span></button></td>";
+		}
+
+		tmEventHTML	+= "</tr>";
 	}
 
 	tmEventHTML += "</tbody>";
 	tmEventHTML += "</table>";
 
 	// Add the Ticketmaster music event information to the web page HTML
-	console.log("tmEventHTML: ", tmEventHTML);
 	$("#event-info").append(tmEventHTML);
 }
 
 // No music event information was returned from Ticketmaster.
-// Display a message on the web page that no music events exist.
 function displayNoEvents() {
 	
-	$(".table").empty();
+	$("#event-info").empty();
 
-	tmEventHTML = "<table class='table'>";
+	tmEventHTML  = "<h2 class='panel-heading'>Ticketmaster Music Events</h2>";
+	tmEventHTML += "<table class='table'>";
 	tmEventHTML += "<thead>";
 	tmEventHTML += "<tr>"
 	tmEventHTML += "<th>Artist Name</th>";
@@ -164,7 +168,69 @@ function displayNoEvents() {
 	$("#event-info").append(tmEventHTML);
 }
 
-// Add the music event to the users list of saved events.
-function saveEvent() {
-	alert("saveEvent() function entered");
+function displaySavedEvent(event) {
+
+	console.log("displaySavedEvent() event: ", event);
+
+	var tmEventId;
+
+	tmEventId = event.currentTarget.attributes[2].nodeValue;
+
+ 	savedEvent = true;
+
+	// tmQueryURL = tmRootQueryURL + "?id=" + tmEventId
+	tmQueryURL = tmRootQueryURL + "?id=alksdfdjf"
+															+ "&apikey=" + tmApiKey;
+
+	console.log("displaySavedEvent() tmQueryURL: ", tmQueryURL);
+
+	$.ajax(
+	{
+	  type:"GET",
+	  url: tmQueryURL,
+	  async:true,
+	  dataType: "json",
+	  success: function(json) {
+
+	  	console.log("displaySavedEvent() Tickmaster JSON: ", json);
+
+	  	// Check if music events were returned
+	  	if (json.page.totalElements === 0) {
+	  		savedEventUnavailable(event);
+	  	}
+		  else {
+	  		displayEvents(json);
+	  	}
+	           },
+	  error: function(xhr, status, err) {
+	              console.log("Ticketmaster AJAX Error!");
+	           }
+	});	
 }
+
+function savedEventUnavailable(event) {
+
+	var tmArtistName;
+
+	tmArtistName = event.currentTarget.attributes[1].nodeValue;
+
+	$("#event-info").empty();
+
+	tmEventHTML =  "<div class='panel-heading'>";
+  tmEventHTML += "<h2 class='panel-title'>Ticketmaster Music Events</h2>";
+  tmEventHTML += "</div>";
+	tmEventHTML += "<table class='table'>";
+	tmEventHTML += "<tbody id='event-schedule'>";
+	tmEventHTML += "<tr>";
+
+	tmEventHTML += "<td>The " + tmArtistName + " music event has started or has already happened so ticket sales have stopped";
+
+	tmEventHTML += "</tr>";
+	tmEventHTML += "</tbody>";
+	tmEventHTML += "</table>";
+
+	// Add the HTML to the web page
+	$("#event-info").append(tmEventHTML);
+}
+
+// End Ticketmaster
